61ce06754281897e5db1c706bf1f4def2ca7d3cc
Kunal <kcunalsharma@gmail,com>
2020-12-18 12:53:41 +0530

    Create adxl_345_zfr.ino
    first commit

---
+++ adxl_345_zfr.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+int ADXL345 = 0x53;
+float X_out, Y_out, Z_out; // outputs
+
+
+void setup() {
+  
+Serial.begin(9600);
+Wire.begin();
+Wire.beginTransmission(ADXL345);
+Wire.write(0x2D);
+Wire.write(8);
+Wire.endTransmission();
+delay(1000);
+}
+
+void loop() {
+
+
+Wire.beginTransmission(ADXL345);
+Wire.write(0x32);
+Wire.endTransmission(false);
+Wire.requestFrom(ADXL345, 6, true);
+X_out = (Wire.read()| Wire.read() << 8);
+X_out = X_out/256;
+Y_out = (Wire.read()| Wire.read() << 8);
+Y_out = Y_out/256;
+Z_out = (Wire.read()| Wire.read() << 8);
+Z_out = Z_out/256;
+
+Serial.print("Xa=");
+Serial.print(X_out);
+
+Serial.print(" \t");
+
+Serial.print("Ya=");
+Serial.print(Y_out);
+
+Serial.print(" \t");
+
+Serial.print("Za=");
+Serial.print(Z_out);
+
+
+Serial. print('\n');
+
+delay(1000);
+
+  
+
+}
